33dd03538f9d9940ab04ce6cdb621aaa205a017d
Destriana Ramadani <destrianaramadani2@gmail.com>
2024-04-24 15:42:31 +0700

    Add files via upload

---
+++ ETL/dags/functions/concat_after_text_processing.py
@@ -0,0 +1,15 @@
+def concatTextProcessingAndBrand():
+    # import library
+    import pandas as pd
+
+    # Read csv file
+    df = pd.read_csv('/opt/airflow/data/text_processing.csv')
+
+    # Concat brand column to'preprocessing_details_category'
+    df['text_processing'] = df['brand']+' '+df['processing_details_category']
+
+    # Select columns
+    df = df[['product_id', 'text_processing']]
+
+    # Save data to csv
+    df.to_csv('/opt/airflow/data/text_processing.csv',index=False)
